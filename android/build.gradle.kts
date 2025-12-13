111ce7bff7375826dafd82643417de046acde1fc
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-12-13 10:34:38 +0530

    change local dir

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,26 +1,3 @@
-// allprojects {
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
-
-// rootProject.buildDir = "../build"
-// subprojects {
-//     project.buildDir = "${rootProject.buildDir}/${project.name}"
-// }
-// subprojects {
-//     project.evaluationDependsOn(":app")
-// }
-
-// tasks.register("clean", Delete) {
-//     delete rootProject.buildDir
-// }
-
-
-// DO NOT declare plugin versions here!
-// Flutter manages Android plugin versions automatically.
-
 allprojects {
     repositories {
         google()
@@ -28,15 +5,19 @@ allprojects {
     }
 }
 
-// Optional: keep or remove this block (custom build directory)
-val newBuildDir = rootProject.layout.buildDirectory.dir("../../build")
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    project.layout.buildDirectory.set(newBuildDir.map { it.dir(project.name) })
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
-// Make sure NOT to include evaluationDependsOn
-// It breaks AGP 8+ and Kotlin DSL
+subprojects {
+    project.evaluationDependsOn(":app")
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
