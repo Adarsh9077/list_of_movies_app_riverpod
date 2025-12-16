669a1fba90bbc2a45cb86ddcd64b4f67bd5d3040
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-12-17 00:01:40 +0530

    add provider in main.dart

--- lib/view_models/theme_provider.dart
+++ lib/view_models/theme_provider.dart
@@ -3,14 +3,17 @@ import 'package:flutter_riverpod/legacy.dart';
 import 'package:mvvm_statemanagements/enums/theme_enums.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+final themeProvider =
+    StateNotifierProvider<ThemeProvider, ThemeEnums>((_) => ThemeProvider());
+
 class ThemeProvider extends StateNotifier<ThemeEnums> {
   final String prefKey = "isDarkMode";
-  ThemeProvider() : super(ThemeEnums.light) {
+  ThemeProvider() : super(ThemeEnums.dark) {
     _loadTheme();
   }
   Future<void> _loadTheme() async {
     final pref = await SharedPreferences.getInstance();
-    final isDarkMode = pref.getBool(prefKey) ?? false;
+    final isDarkMode = pref.getBool(prefKey) ?? true;
     state = isDarkMode ? ThemeEnums.dark : ThemeEnums.light;
   }
 
@@ -19,7 +22,7 @@ class ThemeProvider extends StateNotifier<ThemeEnums> {
     if (state == ThemeEnums.light) {
       state = ThemeEnums.dark;
       await pref.setBool(prefKey, true);
-    }else{
+    } else {
       state = ThemeEnums.light;
       await pref.setBool(prefKey, false);
     }
