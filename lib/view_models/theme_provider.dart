769364216239bfcfbfbc472d670dd3915feeb324
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-25 23:07:13 +0530

    set and get theme enums

--- lib/view_models/theme_provider.dart
+++ lib/view_models/theme_provider.dart
@@ -1,14 +1,32 @@
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:mvvm_statemanagements/enums/theme_enums.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ThemeProvider extends StateNotifier {
+class ThemeProvider extends StateNotifier<ThemeEnums> {
   final prefKey = "isDarkMode";
 
- ThemeProvider():super(){
-   _loadTheme();
- }
- Future<void> _loadTheme() async {
+  ThemeProvider() : super(ThemeEnums.light) {
+    _loadTheme();
+  }
 
- }
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool(prefKey) ?? false;
+    state = isDarkMode ? ThemeEnums.light : ThemeEnums.dark;
+  }
 
+  Future<void> _toggle () async {
+    final pref = await SharedPreferences.getInstance();
+
+    if(state == ThemeEnums.light){
+      state = ThemeEnums.dark;
+      await pref.setBool(prefKey, true);
+    } else {
+      state = ThemeEnums.light;
+      await pref.setBool(prefKey, false);
+    }
+  }
 }
+
+//! 10:30 62 (13min)
