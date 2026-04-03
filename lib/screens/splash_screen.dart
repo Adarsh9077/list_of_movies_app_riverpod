267f93a34750d5b0305d03d8bec2dde5487169b9
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-04-03 23:46:13 +0530

    fetch data on splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -7,46 +7,67 @@ import 'package:mvvm_statemanagements/view_models/movies/movies_provider.dart';
 
 import '../widgets/my_error_widget.dart';
 
+final initializationProvider = FutureProvider((ref) async {
+  // return
+  await Future.microtask(() async {
+    await ref.read(moviesProvider.notifier).getMovies();
+  });
+});
+
 class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
-  Future<void> _loadInitialData(WidgetRef ref) async {
-    await Future.microtask(() async {
-      await ref.read(moviesProvider.notifier).getMovies();
-    });
-  }
+  // Future<void> _loadInitialData(WidgetRef ref) async {
+  //   await Future.microtask(() async {
+  //     await ref.read(moviesProvider.notifier).getMovies();
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // final moviesStateProvider = ref.watch(moviesProvider);
-    return Scaffold(
-      body: FutureBuilder(
-        future: _loadInitialData(ref),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Container(
-                color: Colors.white,
-                child: const Center(child: CircularProgressIndicator()));
-          } else if (snapshot.hasError) {
-            if (ref.watch(moviesProvider).genresList.isNotEmpty) {
-              WidgetsBinding.instance.addPostFrameCallback((_) {
-                getIt<NavigationService>()
-                    .navigateReplace(const MoviesScreen());
-              });
-            }
-            return MyErrorWidget(
-                errorText: snapshot.hasError.toString(),
-                retryFunction: () async {
-                  _loadInitialData(ref);
-                });
-          } else {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              getIt<NavigationService>().navigateReplace(const MoviesScreen());
-            });
-          }
-          return const SizedBox.shrink();
-        },
-      ),
-    );
+    final initWatch = ref.watch(initializationProvider);
+    return initWatch.when(data: (_) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        getIt<NavigationService>().navigateReplace(const MoviesScreen());
+      });
+      return const SizedBox.shrink();
+    }, error: (error, _) {
+      return MyErrorWidget(
+          errorText: error.toString(),
+          retryFunction: () => ref.refresh(initializationProvider));
+    }, loading: () {
+      return const CircularProgressIndicator.adaptive();
+    });
+
+    //   Scaffold(
+    //   body: FutureBuilder(
+    //     future: _loadInitialData(ref),
+    //     builder: (context, snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.waiting) {
+    //         return Container(
+    //             color: Colors.white,
+    //             child: const Center(child: CircularProgressIndicator()));
+    //       } else if (snapshot.hasError) {
+    //         if (ref.watch(moviesProvider).genresList.isNotEmpty) {
+    //           WidgetsBinding.instance.addPostFrameCallback((_) {
+    //             getIt<NavigationService>()
+    //                 .navigateReplace(const MoviesScreen());
+    //           });
+    //         }
+    //         return MyErrorWidget(
+    //             errorText: snapshot.hasError.toString(),
+    //             retryFunction: () async {
+    //               _loadInitialData(ref);
+    //             });
+    //       } else {
+    //         WidgetsBinding.instance.addPostFrameCallback((_) {
+    //           getIt<NavigationService>().navigateReplace(const MoviesScreen());
+    //         });
+    //       }
+    //       return const SizedBox.shrink();
+    //     },
+    //   ),
+    // );
   }
 }
